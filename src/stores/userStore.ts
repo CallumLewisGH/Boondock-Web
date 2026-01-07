8e6a7e7e0a12be475154c924b3e2bfe0117cfc64
Callum Lewis <callumlewis852@gmail.com>
2026-01-07 01:06:37 +0000

    changes authentication service implementation of checkAuth

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -2,7 +2,7 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import { AuthenticationService } from '@/authentication/AuthenticationService'
-import type { user_UserDTO } from '@/api'
+import { UsersService, type user_UserDTO } from '@/api'
 
 export const useUserStore = defineStore('userStore', () => {
   const user = ref<user_UserDTO | null>(null)
@@ -24,10 +24,20 @@ export const useUserStore = defineStore('userStore', () => {
     user.value = null
   }
 
+  async function getCurrentUser() {
+    const res = await UsersService.getUsersMe()
+
+    if (!res.id) {
+      throw new Error('No authenticated user found')
+    }
+
+    return res
+  }
+
   async function checkAuth() {
     loading.value = true
     try {
-      user.value = await AuthenticationService.getCurrentUser()
+      user.value = await getCurrentUser();
       error.value = null
       return true
     } catch (err: any) {
