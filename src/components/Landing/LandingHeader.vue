127d5e915c08fdde39bbb864b3d1e12db2249b5a
Callum Lewis <callumlewis852@gmail.com>
2026-02-12 00:55:02 +0000

    adds burger menu button for mobile

--- src/components/Landing/LandingHeader.vue
+++ src/components/Landing/LandingHeader.vue
@@ -58,11 +58,8 @@
           >
             {{ isDark ? '☀️' : '🌙' }}
           </button>
-          <button @click="isMenuOpen = !isMenuOpen" class="focus:outline-none">
-            <svg class="h-8 w-8" fill="none" viewBox="0 0 24 24" 
-                 :style="{ color: 'var(--accent)' }">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-            </svg>
+          <button style="font-size: x-large;"@click="isMenuOpen = !isMenuOpen" class="focus:outline-none">
+            ☰
           </button>
         </div>
       </div>
